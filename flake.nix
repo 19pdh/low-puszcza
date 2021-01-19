ca9dc0c07dd4970fc8d7d1f45875e8cb0dd357a6
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2021-01-19 18:55:46 +0100

    Add flake

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "19 PDH Puszcza low-tech site";
+  inputs.nur.url = github:nix-community/NUR;
+
+  outputs = { self, nixpkgs, nur }:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; overlays = [ nur.overlay ]; };
+  in {
+    defaultPackage.${system} = self.packages.${system}.low-puszcza;
+
+    packages.${system}.low-puszcza = pkgs.stdenv.mkDerivation {
+      name = "low-puszcza";
+      src = self;
+      nativeBuildInputs = [ pkgs.zip pkgs.nur.repos.pn.saait ];
+
+      installPhase = ''
+        mkdir $out
+        cp -r output $out/
+      '';
+    };
+
+  };
+}
