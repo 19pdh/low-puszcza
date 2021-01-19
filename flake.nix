679b29ec04a08c1ca43bfea6d0f5ac0ae10a72dc
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2021-01-19 20:12:03 +0100

    Add md2saait

--- flake.nix
+++ flake.nix
@@ -6,13 +6,46 @@
   let
     system = "x86_64-linux";
     pkgs = import nixpkgs { inherit system; overlays = [ nur.overlay ]; };
+
+    kronika = pkgs.fetchFromGitHub {
+      repo = "kronika";
+      owner = "19pdh";
+      rev = "master";
+      sha256 = "1pafahvq8xhpba2hgpipkbnhjr3j9zchnxfq4pnxqvidwz5rqr51";
+    };
+
+    md2saait = pkgs.stdenv.mkDerivation {
+      name = "md2saait";
+      src = ./md2saait;
+      installPhase = ''
+        mkdir -p $out/bin
+        cp ./* $out/bin
+        sed -i 's:yq:${pkgs.yq}/bin/yq:g' $out/bin/frontmatter2cfg
+      '';
+    };
+
   in {
     defaultPackage.${system} = self.packages.${system}.low-puszcza;
 
     packages.${system}.low-puszcza = pkgs.stdenv.mkDerivation {
       name = "low-puszcza";
       src = self;
-      nativeBuildInputs = [ pkgs.zip pkgs.nur.repos.pn.saait ];
+      nativeBuildInputs = with pkgs; [
+        md2saait
+        zip
+        pandoc
+        nur.repos.pn.saait
+      ];
+
+      buildPhase = ''
+        cp ${kronika}/wpisy wpisy -r
+        for f in `find wpisy -name *.md`; do
+          name=$(basename $f .md)
+          date=$(getdate $f)
+          frontmatter2cfg $f > pages/$date_$name.cfg
+          pandoc $f > pages/$date_$name.html
+        make
+      '';
 
       installPhase = ''
         cp -r output $out
